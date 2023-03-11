9d6fb144dac7f19b9c027ec3c6beb3255cc7ac13
Andrey Dmitriev <admit8490@gmail.com>
2023-03-11 23:04:46 +0200

    rendering operators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Display from "./components/Display";
 import Digit from "./components/Digit";
+import Operator from "./components/Operator";
 
 import { digits, operators } from "./data";
 
@@ -10,11 +11,21 @@ function App() {
       id={digit.id}
       value={digit.value}
       style = {{gridArea: digit.id}}
-    />)
+      />
+  );
+  const operatorsToRender = operators.map(operator =>
+    <Operator
+      key={operator.id}
+      id={operator.id}
+      value={operator.value}
+      style = {{gridArea: operator.id}}
+    />
+  );
   return (
     <div className="App">
       <Display />
       {digitsToRender}
+      {operatorsToRender}
     </div>
   );
 }
