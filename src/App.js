191a2126a93b6e0b80d82d474aa1b5f7162fcc27
Andrey Dmitriev <admit8490@gmail.com>
2023-03-11 22:55:47 +0200

    digits rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import Display from "./components/Display";
+import Digit from "./components/Digit";
+
+import { digits, operators } from "./data";
+
 function App() {
+  const digitsToRender = digits.map(digit =>
+    <Digit
+      key={digit.id}
+      id={digit.id}
+      value={digit.value}
+      style = {{gridArea: digit.id}}
+    />)
   return (
     <div className="App">
-      <h1>Sanity check</h1>
+      <Display />
+      {digitsToRender}
     </div>
   );
 }
